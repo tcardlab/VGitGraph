aa04246fa776a9e5030afc3151c8d2eab9b3503b
TcardLab <tcardlab@gmail.com>
2019-09-22 19:01:02 -0400

    added linker prefix

--- pages/testing.vue
+++ pages/testing.vue
@@ -48,7 +48,14 @@ export default {
       const coords = yArr.map(i => Array.isArray(i) ? i : [xConst, i])
       return coords
     },
-    
+
+    addLink(link, x, y, scale) {
+        var xprior = link.coord[0]*scale
+        var yprior = link.coord[1]*scale
+        var midY = (yprior + y) / 2;
+        return `M${xprior} ${yprior} C${xprior} ${midY} ${x} ${midY} ${x} ${y}`;
+    },
+
     dString(bItems, display='y', scale = 50) {
       let d = [];
       var coords = this.getCoords(bItems)
@@ -56,26 +63,34 @@ export default {
 
       for (var i of _.range(coords.length)) {
         let [x, y] = coords[i]
+
+        var link = bItems.path[i].link
+        var dLink = ''
+        if (Object.keys(link).length > 0){
+          // dStrting link can be prefixed to any point 
+          dLink = this.addLink(link, x, y, scale);
+        }
+
         if (d.length === 0) {
           // 'move-to' origin
-          d = d.concat(`M${x} ${y}`);
+          d = d.concat(dLink+`M${x} ${y}`);
         } else if (x - coords[i-1][0] !== 0) {
           // 'branch/merge' off prior
           var [xprior, yprior] = coords[i - 1];
           var midY = (yprior + y) / 2;
-          d = d.concat(`C${xprior} ${midY} ${x} ${midY} ${x} ${y}`);
+          d = d.concat(dLink+`C${xprior} ${midY} ${x} ${midY} ${x} ${y}`);
         } else if (y - coords[i - 1][1] > 1 * scale) {
           // discontinuity
-          d = d.concat(`M${x} ${y}`);
+          d = d.concat(dLink+`M${x} ${y}`);
         } else {
           // vertical Line
           let last_index = d.length-1
           if (d[last_index][0] === "L") {
             // Extend prior line
-            d[last_index] = (`L${x} ${y}`)
+            d[last_index] = (dLink+`L${x} ${y}`)
           } else {
             // Start new line
-            d = d.concat(`L${x} ${y}`)
+            d = d.concat(dLink+`L${x} ${y}`)
           }
         }
       }
--- store/index.js
+++ store/index.js
@@ -1,95 +1,94 @@
 export const strict = false;
 
-export const state = () => ({
-  branches1: {
-    Branch1: {
-      0: { Coord: [0, 0], time: "0", glyph: "book", event: "origin link" },
-      1: { Coord: [1, 1], time: "1", glyph: "book", event: "action1" },
-      2: { Coord: [1, 2], time: "0", glyph: "book", event: "action2" },
-      3: { Coord: [1, 3], time: "0", glyph: "book", event: "action2" },
-      4: { Coord: [1, 5], time: "0", glyph: "book", event: "action2" },
-      5: { Coord: [1, 6], time: "0", glyph: "book", event: "action2" }
-    },
-    Branch2: {
-      0: { Coord: [0, 0], time: "0", glyph: "book", event: "origin link" },
-      1: { Coord: [-1, 1], time: "1", glyph: "book", event: "action1" },
-      2: { Coord: [-1, 2], time: "0", glyph: "book", event: "action2" },
-      3: { Coord: [1, 6], time: "0", glyph: "book", event: "action2" }
-    },
-    Branch3: {
-      0: { Coord: [0, 0], time: "0", glyph: "book", event: "origin link" },
-      1: { Coord: [-2, 1], time: "1", glyph: "book", event: "action1" },
-      2: { Coord: [-2, 2], time: "0", glyph: "book", event: "action2" }
-    }
-  },
-  branches2: {
-    "-1": {
-      origin: [0, 0], // off characters branch, ie not his action. so no data?
-      path: {
-        1: {
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: "get-self-origin", type: "branch" }
-        },
-        2: {
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: [0, 0], type: "time jump" }
-        },
-        4: {
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: "terminus", type: "merge" }
-        }
-      },
-      terminus: [3, 7]
-    }
-  },
+export const state = () => ({  
   branches3: {
     "GM": {
       x: [-1],
       dx: 0,
       children: [],
       path: [
-        { turn: 0,
-          y: [0,0],
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: "get-self-origin", type: "branch" }
+        { turn: 0, y: [0,0], unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
         },
-        { turn: 0,
-          y: 1,
-          unix: 1569580240,
+        { turn: 0, y: 1, unix: 1569580240,
           glyph: "book",
           event: "link",
-          link: { coord: "get-self-origin", type: "branch" }
+          link: { }
         },
-        { turn: 0,
-          y: 2,
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: "get-self-origin", type: "branch" }
+        { turn: 0, y: 2, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
         },
-        { turn: 0,
-          y: 3,
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: "get-self-origin", type: "branch" }
+        { turn: 0, y: 3, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
         },
-        { turn: 0,
-          y: 5,
-          unix: 1569580240,
-          glyph: "book",
-          event: "link",
-          link: { coord: "get-self-origin", type: "branch" }
+        { turn: 0, y: 5, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
         }
       ],
-    }
+    },
+    "P1": {
+      x: [1],
+      dx: 0,
+      children: [],
+      path: [
+        { turn: 0, y: [0,0], unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        },
+        { turn: 0, y: 1, unix: 1569580240,
+          glyph: "book",
+          event: "link",
+          link: { }
+        },
+        { turn: 0, y: [2, 2], unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        },
+        { turn: 0, y: [2,3], unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        },
+        { turn: 0, y: [1,4], unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        },
+        { turn: 0, y: [1,5], unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        }
+      ],
+    },
+    "GM2": {
+      x: [-2],
+      dx: 0,
+      children: [],
+      path: [
+        { turn: 0, y: 2, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { coord: [-1, 1], type: "branch" }
+        },
+        { turn: 0, y: 3, unix: 1569580240,
+          glyph: "book",
+          event: "link",
+          link: { }
+        },
+        { turn: 0, y: 4, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        },
+        { turn: 0, y: 5, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        },
+        { turn: 0, y: 6, unix: 1569580240,
+          glyph: "book", event: "link",
+          link: { }
+        }
+      ],
+    },
   }
 });
