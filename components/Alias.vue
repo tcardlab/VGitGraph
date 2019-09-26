4f159b8b9e5974b466e59e29314dbdd94e3e919d
TcardLab <tcardlab@gmail.com>
2019-09-26 00:53:31 -0400

    Demonestrates Functionality
    This is either the dumbest or smartest code I have ever written. It loops through id's and renders an ailias later with a new z-index.
    
    While it is condensed and guarantees glyphs are rendered on top, It does not guarentee that links will be.
    
    I will try making an alias template to make this cleaner,  but its begining to get a bit round about.
    
    It might simply be cleaner, faster, and more intuitive to leave as explicit loops for each. hence the new branch.

---
+++ components/Alias.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg :id="svgId+'-alias'">
+    <g :id="'alias-'+branchName" v-for="(items, branchName) in _$" 
+        :key="'alias-'+branchName">
+      <use
+        v-for="(i, turn) in items.path" :key="turn"
+        :href="'#'+svgId+'-'+turn"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props:['svgId'],
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,12 @@
     </div>
 
     <!--stuff inside svg to be moved to a seperate component-->
-    <svg overflow="visible">
+    <svg overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <!-- 
         It looks as if they can be combined, however it will
         mess up svg render order as there is no z-index...
       -->
-      <Paths
+      <!-- <Paths
         v-for="(items, branchName) in _$" :key="'path-'+branchName"
         :items="items"
       />
@@ -29,7 +29,27 @@
           v-for="(actions, turn) in items.path" :key="turn"
           :items="items" :i="actions" :turn="turn"
         />
+      </g> -->
+
+
+      <!-- -->
+      <g v-for="(items, branchName) in _$" :key="branchName">
+        <Paths :items="items"/>
+        <g v-for="(i, turn) in items.path" :key="turn">
+          <Links :items="items" :i="i" :turn="turn" :id="'link-'+turn"/>
+          <Glyphs :items="items" :i="i" :turn="turn" :id="'glyph-'+turn"/>
+        </g>
       </g>
+      <!-- <Alias svgId="link"/> -->
+      <!-- <Alias svgId="glyph"/> -->
+      <g :id="'alias-'+branchName" v-for="(items, branchName) in _$" 
+         :key="'alias-'+branchName">
+        <use
+          v-for="(i, turn) in items.path" :key="turn"
+          :href="'#glyph-'+turn"
+        />
+      </g>
+
     </svg>
 
   </div>
@@ -39,12 +59,14 @@
 import Paths from "~/components/Paths.vue";
 import Links from "~/components/Links.vue";
 import Glyphs from "~/components/Glyphs.vue";
+import Alias from "~/components/Alias.vue";
 
 export default {
   components: {
     Paths,
     Links,
-    Glyphs
+    Glyphs,
+    Alias
   },
 
   data() {
