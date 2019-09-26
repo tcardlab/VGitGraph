ad291c70cd5f89d56289dd4c4346c3a9716bc022
TcardLab <tcardlab@gmail.com>
2019-09-26 01:12:43 -0400

    Cleaned but wont pull
    While it does function, it failed to achieve my intentions– reducing for loops and improving clarity. (condensing is inferior to these)
    
    This unfortunately doesnt impove clarity, it hides the loops and muddies the final HTML.
    
    I may as well have created a component to outsource the for loops prior to branching. That would have been more direct than use at least.

--- pages/index.vue
+++ pages/index.vue
@@ -8,31 +8,6 @@
 
     <!--stuff inside svg to be moved to a seperate component-->
     <svg overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <!-- 
-        It looks as if they can be combined, however it will
-        mess up svg render order as there is no z-index...
-      -->
-      <!-- <Paths
-        v-for="(items, branchName) in _$" :key="'path-'+branchName"
-        :items="items"
-      />
-
-      <g v-for="(items, branchName) in _$" :key="'link-'+branchName">
-        <Links
-          v-for="(actions, turn) in items.path" :key="turn" 
-          :items="items" :i="actions" :turn="turn"
-        />
-      </g>
-
-      <g v-for="(items, branchName) in _$" :key="'glyph-'+branchName">
-        <Glyphs
-          v-for="(actions, turn) in items.path" :key="turn"
-          :items="items" :i="actions" :turn="turn"
-        />
-      </g> -->
-
-
-      <!-- -->
       <g v-for="(items, branchName) in _$" :key="branchName">
         <Paths :items="items"/>
         <g v-for="(i, turn) in items.path" :key="turn">
@@ -40,18 +15,10 @@
           <Glyphs :items="items" :i="i" :turn="turn" :id="'glyph-'+turn"/>
         </g>
       </g>
+      <!--Assign new Z-Index-->
       <Alias svgId="link"/>
       <Alias svgId="glyph"/>
-      <!-- <g v-for="(items, branchName) in _$" :id="'alias-'+branchName" 
-         :key="'alias-'+branchName">
-        <use
-          v-for="(i, turn) in items.path" :key="turn"
-          :href="'#glyph-'+turn"
-        />
-      </g> -->
-
     </svg>
-
   </div>
 </template>
 
