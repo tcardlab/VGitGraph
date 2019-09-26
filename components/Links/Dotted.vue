7b5ee62a86bac95036fd09bd4ff4a64482472b0e
TcardLab <tcardlab@gmail.com>
2019-09-26 03:58:07 -0400

    add comment on relevance to links

--- components/Links/Dotted.vue
+++ components/Links/Dotted.vue
@@ -7,6 +7,7 @@ export default {
   props: ['x', 'xLink','y', 'yLink',  'color'],
   methods: {
     dString(x, y, xLink, yLink) {
+      // could import PathsMixin for shared elements.
       return `M${x} ${y} L${xLink} ${yLink}`
     }
   }
