c2f8fb6576398b9242ccc216ad5d3a8dcc9da581
TcardLab <tcardlab@gmail.com>
2019-11-07 13:44:05 -0500

    dropdown search bar

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,26 @@
 
     <VGitGraph/>
 
+  <div class="dropdown">
+    <input 
+      class="dropdown-input"
+      @focus="initQuery()" @blur="dropdown=false"
+      @keyup.enter="selectOption(filteredList[0])"
+      type="text" v-model="search"
+      placeholder="Search branches..." />
+    <transition-group name="fade" tag="div"
+      class="dropdown-content" v-show="dropdown"
+      >
+      <div
+        class="dropdown-item"
+        @mousedown="selectOption(option)"
+        v-for="(option, index) in filteredList"
+        :key="option" :style="'transition: all .3s '+300*index/(filteredList.length)+'ms'">
+          {{ option }}
+      </div>
+    </transition-group>
+  </div>
+
   </div>
 </template>
 
@@ -28,5 +48,62 @@ export default {
     VGitGraph,
     CollapseState
   },
+  computed: {
+    filteredList() {
+      if(this.selected){
+        return [this.selected]
+      } else {
+        return Object.keys(this._$).filter(k => {
+          return k.toLowerCase().includes(this.search.toLowerCase())
+        })
+      }
+    }
+  },
+  methods:{
+    selectOption(option) {
+      this.search = option
+      this.selected = option
+      this.dropdown=false
+    },
+    initQuery(){
+      this.dropdown = true
+      this.selected = false
+    }
+  },
+  data:() => {
+    return {
+      dropdown: false,
+      search:'',
+      selected:false
+    }
+  }
 }
 </script>
+
+<style>
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+.fade.enter{
+  opacity: 1;
+}
+
+.dropdown{
+  width: 200px;
+}
+.dropdown-input{
+  width: 100%;
+  position: relative;
+}
+.dropdown-content{
+  z-index: 100;
+  background-color: azure;
+  position: relative;
+}
+.dropdown-item:hover{
+  z-index: 101;
+  transition-duration: .3s;
+  transition-delay: 50ms !important;
+  background-color: cornflowerblue;
+}
+</style>
